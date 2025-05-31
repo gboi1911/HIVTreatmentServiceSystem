4e71a4f55c2748f78509de2aa1db5bdf6d620495
nguyendang03 <npmd2004@gmail.com>
2025-05-31 16:55:13 +0700

    move home components to home.jsx

--- HIVTreatment/src/components/Layout.jsx
+++ HIVTreatment/src/components/Layout.jsx
@@ -2,22 +2,14 @@ import React from "react";
 import Navbar from "./Navbar";
 import AppFooter from "./Footer";
 import { Outlet } from "react-router-dom";
-import NewsSection from "./HomePage/NewsSection";
-import TechSection from "./HomePage/TechSection";
-import NewTechSection from "./HomePage/NewTechSection";
-import CategorySection from "./HomePage/CategorySection";
-import NavFooter from "./HomePage/NavFooter";
+
 const MainLayout = () => {
   return (
     <div>
       <Navbar />
       <main>
         <Outlet />
-        <NewsSection/>
-        <NewTechSection/>
-        <TechSection/>
-        <CategorySection/>
-        <NavFooter/>
+        
       </main>
       <AppFooter />
     </div>
--- HIVTreatment/src/pages/Home.jsx
+++ HIVTreatment/src/pages/Home.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import Hero from "../components/HeroSection";
 import CardSection from "../components/Cards";
-
+import NewsSection from "../components/HomePage/NewsSection";
+import TechSection from "../components/HomePage/TechSection";
+import NewTechSection from "../components/HomePage/NewTechSection";
+import CategorySection from "../components/HomePage/CategorySection";
+import NavFooter from "../components/HomePage/NavFooter";
 export default function Home() {
   return (
     <>
       <Hero />
       <div className="w-full flex justify-center px-4 overflow-x-auto">
         <CardSection />
+
       </div>
+              <NewsSection/>
+                <NewTechSection/>
+                <TechSection/>
+                <CategorySection/>
+                <NavFooter/>
     </>
   );
 }
